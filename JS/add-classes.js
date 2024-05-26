f65208fa21a3eada7980d046c535881a226d4f52
James <jkeezer@pm.me>
2024-05-26 17:16:56 -0400

    v1.0.11 - JS to apply classes to curriculum cards

--- JS/add-classes.js
+++ JS/add-classes.js
@@ -11,15 +11,21 @@ function applyStyles() {
     'span',
     'Free course completed!'
   );
+
   const regularCourses = getElementsByTextContent('span', 'Course');
   const completedRegularCourses = getElementsByTextContent(
     'span',
     'Course completed!'
   );
-  const skillPaths = getElementsByTextContent('span', 'Skill Path');
+  const skillPaths = getElementsByTextContent('span', 'Skill path');
   const completedSkillPaths = getElementsByTextContent(
     'span',
     'Skill Path completed!'
+  );
+  const careerPaths = getElementsByTextContent('span', 'Career path');
+  const completedCareerPaths = getElementsByTextContent(
+    'span',
+    'Career Path completed!'
   );
 
   freeCourses.forEach((element) => {
@@ -40,10 +46,40 @@ function applyStyles() {
   completedSkillPaths.forEach((element) => {
     element.parentElement.classList.add('completedSkillPath');
   });
+  careerPaths.forEach((element) => {
+    element.parentElement.classList.add('careerPath');
+  });
+  completedCareerPaths.forEach((element) => {
+    element.parentElement.classList.add('completedCareerPath');
+  });
+}
+
+function init() {
+  // Check if we are on the homepage
+  if (
+    window.location.pathname === '/' ||
+    window.location.pathname.startsWith('/catalog') ||
+    window.location.pathname.startsWith('/learn')
+  ) {
+    applyStyles();
+
+    // Create a MutationObserver to watch for changes in the DOM
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        applyStyles();
+      });
+    });
+
+    // Start observing the document for changes
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  }
 }
 
 if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', applyStyles);
+  document.addEventListener('DOMContentLoaded', init);
 } else {
-  applyStyles();
+  init();
 }
